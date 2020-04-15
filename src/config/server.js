de19afef3cc03e40883a559eff7dfc2c3afa573b
Carol Vasconcelos <cvsouza04@gmail.com>
2020-04-15 16:25:56 -0300

    Habilitando o CORS atraves da lib

--- src/config/server.js
+++ src/config/server.js
@@ -2,13 +2,14 @@ const port = 4003
 
 const bodyParser = require('body-parser')
 const express = require('express')
+const cors = require('cors');
 const server = express()
-const allowCors = require('./cors')
+// const allowCors = require('./cors')
 const queryParser = require('express-query-int')
 
 server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
-server.use(allowCors)
+server.use(cors());
 server.use(queryParser())
 
 server.listen(port, function () {
