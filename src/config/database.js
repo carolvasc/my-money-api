11d1be671d2d13319e6a3b87b7b7065b846fb261
Carol Vasconcelos <cvsouza04@gmail.com>
2020-04-28 09:58:46 -0300

    Configuração do MONGOLAB_URI

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
-module.exports = mongoose.connect('mongodb://localhost/mymoney')
+const url = process.env.MONGOLAB_URI ? process.env.MONGOLAB_URI : 'mongodb://localhost/mymoney'
+module.exports = mongoose.connect(url, { useMongoClient: true })
 
 mongoose.Error.messages.general.required = "O atributo {PATH} é obrigatório."
 mongoose.Error.messages.Number.min = "O '{VALUE}' informado é menor que o limite mínimo de '{MIN}'."
